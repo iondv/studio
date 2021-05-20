53476d86e86e37f01c74cbe9526a96a892ba1c3c
sovereign <allys@live.ru>
2021-05-20 13:34:45 +1000

    IONEXAMPLE-196 xls в варианте с фреймворком

---
+++ services/dbToApp.js
@@ -0,0 +1,32 @@
+const Service = require('modules/rest/lib/interfaces/Service');
+const appMaker = require('../backend/xls/appMaker');
+
+function DbToMetadata(options) {
+  this._route = function(router) {
+    /**
+     * @param {Request} req
+     * @returns {Promise}
+     * @private
+     */
+    this.addHandler(router, '/xls', 'PUT', (req) => {
+      const chunks = [];
+      return new Promise((resolve) => {
+        req.on('data', (chunk) => {
+          chunks.push(chunk);
+        });
+        req.on('end', async () => {
+          const source = Buffer.concat(chunks);
+          const appName = 'generated-ion-app';
+          const app = new appMaker(appName);
+          const zip = await (app).fromXls(source);
+          await app.clean();
+          resolve(zip);
+        });
+      });
+    });
+  };
+}
+
+DbToMetadata.prototype = new Service();
+
+module.exports = DbToMetadata;
